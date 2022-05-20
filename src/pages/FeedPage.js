868473feb3dc558094019131a9188c337dc3365e
Duarte Carmo <duarteocarmo@gmail.com>
2022-05-20 09:03:28 +0100

    chapter 4

---
+++ src/pages/FeedPage.js
@@ -0,0 +1,10 @@
+import Posts from "../components/Posts";
+import Body from "../components/Body";
+
+export default function FeedPage() {
+  return (
+    <Body sidebar>
+      <Posts />
+    </Body>
+  );
+}
